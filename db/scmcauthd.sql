fd7e2a1cef07d4bbab4a0b0e2cfbaab928b668a6
Felix Jacobi <felix.jacobi@stsbl.de>
2017-06-29 16:13:21 +0200

    sql: Limit update privilege for scmcauthd.

--- db/scmcauthd.sql
+++ db/scmcauthd.sql
@@ -1,5 +1,7 @@
 CREATE USER scmcauthd;
 
 GRANT SELECT ON "users_priv", "privileges", "users", "groups", "role_privileges", "user_roles" TO "scmcauthd";
-GRANT SELECT, INSERT, UPDATE ON "session_log" TO "scmcauthd";
+
+GRANT SELECT, INSERT ON "session_log" TO "scmcauthd";
+GRANT UPDATE (client_agent, logout, logout_reason) ON session_log TO scmcauthd;
 GRANT SELECT, USAGE ON "session_log_id_seq" TO "scmcauthd";
